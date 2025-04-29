cac5668c9c2ba1def025f196d84741ba21d9e530
Kutay K. <kutaykeless@gmail.com>
2025-04-29 22:35:49 +0300

    finish multi class logisctic regression

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -12,21 +12,21 @@ class LogisticRegression:
     def __init__(self, X, y, random_state=None):
         self.X = X
         self.y = y
-        self.n_classes = len(np.unique(y))
+        self.n_classes = np.unique(y)
         n_features = X.shape[1]
         if random_state is not None:
             np.random.seed(random_state)  # <-- only setting the seed
 
-        if self.n_classes == 2:
+        if len(self.n_classes) == 2:
             self.w = np.random.randn(n_features) * 0.01
             self.b = 0.0
         else:
-            self.w = np.random.randn(n_features, self.n_classes) * 0.01
-            self.b = np.zeros(self.n_classes)  # Bias also per class        
+            self.w = np.random.randn(n_features, len(self.n_classes)) * 0.01
+            self.b = np.zeros(len(self.n_classes))  # Bias also per class        
 
     def predict_proba(self, X):
         z = self.calc_linear_combination(X)
-        if(self.n_classes == 2):
+        if(len(self.n_classes) == 2):
             sigmoid = self.calc_sigmoid(z)
             return sigmoid
         else:
@@ -43,30 +43,53 @@ class LogisticRegression:
             self.b = self.b - learning_rate * self.calc_gradient_bias(z)
 
             if i % 1000 == 0 and verbose == True:
-                print(self.calc_binary_cross_entropy(z))
-
-    def predict(self, X):
-        comb = self.calc_linear_combination(X)
-        sigmoid = self.calc_sigmoid(comb)
-        return sigmoid
+                if (len(self.n_classes) == 2):
+                    print(self.calc_binary_cross_entropy(z))
+                else:
+                    print(self.calc_multi_class_cross_entropy(z))
 
     def calc_gradient_weight(self, z):
         m = self.X.shape[0]
-        errors = z - self.y
-        return (1 / m) * np.dot(self.X.T, errors)
+        #binary classification
+        if(len(self.n_classes) == 2):
+            errors = z - self.y
+            return (1 / m) * np.dot(self.X.T, errors)
+        else:
+            tmp = z.copy()
+            counter = 0
+            for y, y_ in zip(self.y, tmp):
+                y_[y] = y_[y] - 1
+                tmp[counter] = y_
+                counter+=1
+            return (1 / m) * np.dot(self.X.T, tmp)
 
     def calc_gradient_bias(self, z):
-        row = self.X.shape[0]
-        sum_ = 0
-        for i in range(row):
-            sum_ += z[i] - self.y[i]
-        return (1 / row) * sum_
+        m = self.X.shape[0]
+        if len(self.n_classes) == 2:
+            sum_ = 0
+            for i in range(m):
+                sum_ += z[i] - self.y[i]
+            return (1 / m) * sum_
+        else:
+            tmp = z.copy()
+            counter = 0
+            for y, y_ in zip(self.y, tmp):
+                y_[y] = y_[y] - 1
+                tmp[counter] = y_
+                counter+=1
+            return (1 / m) * np.sum(tmp, axis=0)
 
     def calc_binary_cross_entropy(self, y_):
         total_loss = 0
         for i,j in zip(self.y, y_):
             total_loss += -(i * np.log(j) + (1 - i) * np.log(1-j))
         return total_loss / self.y.shape[0]
+    
+    def calc_multi_class_cross_entropy(self, y_):
+        total_loss = 0
+        for i,j in zip(self.y, y_):
+            total_loss += -np.log(j[i])
+        return total_loss / len(self.y)
 
     def calc_linear_combination(self, X):
         return np.dot(X, self.w) + self.b
@@ -85,9 +108,6 @@ class LogisticRegression:
         return sf_result
 
 
-
-
-
 if __name__ == "__main__":
     p = pd.read_csv("Cancer.csv").fillna(0)
     np.random.seed(42)
@@ -99,5 +119,5 @@ if __name__ == "__main__":
     train_X = p.drop("diagnosis",axis=1).values
     train_y = p["diagnosis"].values
 
-    res = LogisticRegression(train_X,train_y)
-    print(res.predict_proba(train_X))
+    l = LogisticRegression(train_X,train_y)
+    l.fit()
